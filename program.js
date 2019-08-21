5c98e15992e10af65905daadc69600da2f36f93d
Dzesika Tkaczyk <dzesika.tkaczyk@gmail.com>
2019-08-21 22:45:15 +0200

    node

---
+++ program.js
@@ -0,0 +1,22 @@
+process.stdin.setEncoding('utf-8');
+
+process.stdin.on('readable', function() {
+    var input = process.stdin.read();
+
+    if(input !== null) {
+    	var instruction = input.toString().trim();
+    	switch (instruction) {
+	    	case '/exit':
+	    		process.stdout.write('Quitting app!\n');
+	    		process.exit();
+	    	case '/node':
+				process.stdout.write(process.versions.node);
+				break;
+	    	case '/sysLang':
+	    		process.stdout.write(process.env.LANG);
+	    		break;
+	    	default:
+	    		process.stderr.write('Wrong instruction!\n')
+	    }
+    }
+});
