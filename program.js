36c4057296b18fe2e4ed571d82aa483250c3c96d
Dzesika Tkaczyk <dzesika.tkaczyk@gmail.com>
2019-08-22 22:32:16 +0200

    change input

--- program.js
+++ program.js
@@ -3,18 +3,23 @@ process.stdin.setEncoding('utf-8');
 process.stdin.on('readable', function() {
     var input = process.stdin.read();
 
+    if (!input) {
+    	return
+    }
+    
 	var instruction = input.toString().trim();
 	switch (instruction) {
     	case '/exit':
     		process.stdout.write('Quitting app!\n');
     		process.exit();
     	case '/node':
-		process.stdout.write(process.versions.node);
-		break;
+			process.stdout.write(process.versions.node);
+			break;
     	case '/sysLang':
     		process.stdout.write(process.env.LANG);
     		break;
     	default:
     		process.stderr.write('Wrong instruction!\n')
     }
+	
 });
