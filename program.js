192fe7c30b23298bce3f494db48bee9692bd28e4
DzesikaTkaczyk <50298851+DzesikaTkaczyk@users.noreply.github.com>
2019-08-25 21:54:37 +0200

    Update program.js

--- program.js
+++ program.js
@@ -2,19 +2,18 @@ process.stdin.setEncoding('utf-8');
 
 process.stdin.on('readable', function() {
 	var input = process.stdin.read();
-
-    if (!input) {
-    	return
-    }
-    
+	
+	if (!input) {
+		return
+	}
 	var instruction = input.toString().trim();
 	switch (instruction) {
     	case '/exit':
     		process.stdout.write('Quitting app!\n');
     		process.exit();
     	case '/node':
-			process.stdout.write(process.versions.node);
-			break;
+		process.stdout.write(process.versions.node);
+		break;
     	case '/sysLang':
     		process.stdout.write(process.env.LANG);
     		break;
